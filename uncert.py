5d0fc78f6d1e51100efb436f35a02551d663671b
Darren Tan <dyztan@gmail.com>
2018-04-13 00:00:12 -0500

    added subtract, divide

--- uncert.py
+++ uncert.py
@@ -1,38 +1,70 @@
-'''testing out uncertainties module'''
+''' Basic math operations with uncertainties '''
 
 from uncertainties import ufloat_fromstr
 
 
+def add_uncertainty():
+    ''' add two values together and find uncertainty'''
+    input_value = get_input(2)
+    output = input_value[0] + input_value[1]
+    print(output)
+    return output
+
+
+def subtract_uncertainty():
+    '''subtract two values together and find uncertainty'''
+    input_value = get_input(2)
+    output = input_value[0] - input_value[1]
+    print(output)
+    return output
+
+
 def multiply_uncertainty():
     '''multiplies two values and find uncertainty'''
-    input_x = get_input()
-    input_y = get_input()
-    output = input_x * input_y
+    input_value = get_input(2)
+    output = input_value[0] * input_value[1]
+    print(output)
+    return output
+
+
+def divide_uncertainty():
+    '''divide two values and find uncertainty'''
+    input_value = get_input(2)
+    output = input_value[0] / input_value[1]
     print(output)
     return output
 
 
 def square_uncertainty():
     '''squares value and find uncertainty'''
-    input_x = get_input()
-    output = input_x**2
+    input_value = get_input(1)
+    output = input_value[0]**2
     print(output)
     return output
 
 
-def get_input():
-    '''get user input and format to uncertainty'''
-    user_input_main = input('Input main value >>> ')
-    user_input_uncertainty = input('Input uncertainty value >>> ')
-    input_value = ufloat_fromstr(
-        user_input_main + '+/-' + user_input_uncertainty)
+def get_input(num):
+    ''' 
+    Asks user for value and uncertainty
+    Input = # of values to collect
+    Output = Array of values formatted for uncertainty library
+    '''
+
+    input_value = []
+    for i in range(1, num+1):
+        user_input_value = input('Input value ' + str(i) + ' >>> ')
+        user_input_uncertainty = input(
+            'Input uncertainty value ' + str(i) + ' >>> ')
+        input_value.append(ufloat_fromstr(
+            user_input_value + '+/-' + user_input_uncertainty))
     return input_value
 
 
 def main():
     ''' main function '''
-    square_uncertainty()
-    multiply_uncertainty()
+    add_uncertainty()
+    # square_uncertainty()
+    # multiply_uncertainty()
 
 
 if __name__ == '__main__':

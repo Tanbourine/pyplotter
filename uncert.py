37c869d3b2082bfa07c416b95d96e0f08d0ad1a0
Darren Tan <dyztan@gmail.com>
2018-04-13 00:13:20 -0500

    formatting output strings

--- uncert.py
+++ uncert.py
@@ -2,53 +2,64 @@
 
 from uncertainties import ufloat_fromstr
 
+def format_output(output):
+    ''' formats output string to be pleasing to the eye'''
+    print('')
+    print('==============')
+    print(output)
+    print('==============')
+    print('')
 
 def add_uncertainty():
     ''' add two values together and find uncertainty'''
+    print("Adding two values with uncertainties")
     input_value = get_input(2)
     output = input_value[0] + input_value[1]
-    print(output)
+    format_output(output)
     return output
 
 
 def subtract_uncertainty():
     '''subtract two values together and find uncertainty'''
+    print("Subtracting two values with uncertainties")
     input_value = get_input(2)
     output = input_value[0] - input_value[1]
-    print(output)
+    format_output(output)
     return output
 
 
 def multiply_uncertainty():
     '''multiplies two values and find uncertainty'''
+    print("Multiplying two values with uncertainties")
     input_value = get_input(2)
     output = input_value[0] * input_value[1]
-    print(output)
+    format_output(output)
     return output
 
 
 def divide_uncertainty():
     '''divide two values and find uncertainty'''
+    print("Dividing two values with uncertainties")
     input_value = get_input(2)
     output = input_value[0] / input_value[1]
-    print(output)
+    format_output(output)
     return output
 
 
 def square_uncertainty():
     '''squares value and find uncertainty'''
+    print("Square a value with uncertainties")
     input_value = get_input(1)
     output = input_value[0]**2
-    print(output)
+    format_output(output)
     return output
 
 
 def get_input(num):
-    ''' 
-    Asks user for value and uncertainty
+    """ Asks user for value and uncertainty
     Input = # of values to collect
     Output = Array of values formatted for uncertainty library
-    '''
+    """
 
     input_value = []
     for i in range(1, num+1):
@@ -63,8 +74,10 @@ def get_input(num):
 def main():
     ''' main function '''
     add_uncertainty()
-    # square_uncertainty()
-    # multiply_uncertainty()
+    subtract_uncertainty()
+    multiply_uncertainty()
+    divide_uncertainty()
+    square_uncertainty()
 
 
 if __name__ == '__main__':

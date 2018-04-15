41a12fdae8eed632adf148924bc4cb5a67c29332
Darren Tan <dyztan@gmail.com>
2018-04-15 12:59:18 -0500

    chaining multiple operations together

--- uncert.py
+++ uncert.py
@@ -1,4 +1,4 @@
-''' Basic math operations with uncertainties '''
+""" Basic math operations with uncertainties """
 
 from uncertainties import ufloat_fromstr
 
@@ -11,106 +11,108 @@ def format_output(output):
     print('')
 
 
-def add_uncertainty():
+def add_uncertainty(propogated_value):
     ''' add two values together and find uncertainty'''
     print("Adding two values with uncertainties")
-    input_value = get_input(2)
-    output = input_value[0] + input_value[1]
+    input_value = get_input()
+    output = propogated_value + input_value
     format_output(output)
     return output
 
 
-def subtract_uncertainty():
+def subtract_uncertainty(propogated_value):
     '''subtract two values together and find uncertainty'''
     print("Subtracting two values with uncertainties")
-    input_value = get_input(2)
-    output = input_value[0] - input_value[1]
+    input_value = get_input()
+    output = propogated_value - input_value
     format_output(output)
     return output
 
 
-def multiply_uncertainty():
+def multiply_uncertainty(propogated_value):
     '''multiplies two values and find uncertainty'''
     print("Multiplying two values with uncertainties")
-    input_value = get_input(2)
-    output = input_value[0] * input_value[1]
+    input_value = get_input()
+    output = propogated_value * input_value
     format_output(output)
     return output
 
 
-def divide_uncertainty():
+def divide_uncertainty(propogated_value):
     '''divide two values and find uncertainty'''
     print("Dividing two values with uncertainties")
-    input_value = get_input(2)
-    output = input_value[0] / input_value[1]
+    input_value = get_input()
+    output = propogated_value / input_value
     format_output(output)
     return output
 
 
-def square_uncertainty():
+def square_uncertainty(propogated_value):
     '''squares value and find uncertainty'''
     print("Square a value with uncertainties")
-    input_value = get_input(1)
-    output = input_value[0]**2
+    # input_value = get_input()
+    output = propogated_value**2
     format_output(output)
     return output
 
 
-def get_input(num):
+def get_input():
     """ Asks user for value and uncertainty
     Input = # of values to collect
     Output = Array of values formatted for uncertainty library
     """
 
-    input_value = []
-    for i in range(1, num+1):
-        print('')
-        user_input_value = input('Input value ' + str(i) + '                 >>> ')
-        user_input_uncertainty = input(
-            'Input uncertainty value ' + str(i) + '     >>> +/-  ')
-        print('')
-        input_value.append(ufloat_fromstr(
-            user_input_value + '+/-' + user_input_uncertainty))
+    # input_value = []
+    # for i in range(1, num+1):
+    print('')
+    user_input_value = input(
+        'Input value ' + '                 >>> ')
+    user_input_uncertainty = input(
+        'Input uncertainty value ' +  '     >>> +/-  ')
+    print('')
+    input_value = (ufloat_fromstr(
+        user_input_value + '+/-' + user_input_uncertainty))
     return input_value
 
 
 def main():
     ''' main function '''
+    add_uncertainty(multiply_uncertainty(get_input()))
 
-    flag = 'true'
-    while flag in ['true', 'yes', 'y', 'yup', 'yessir', 'yeah', '', ' ']:
-        operation = input('Which operation would you like to perform? >>> ')
-        operation = operation.lower()
+    # flag = 'true'
+    # while flag in ['true', 'yes', 'y', 'yup', 'yessir', 'yeah', '', ' ']:
+    # operation = input('Which operation would you like to perform? >>> ')
+    # operation = operation.lower()
 
-        print('')
-        print('==============')
-        print('')
+    # print('')
+    # print('==============')
+    # print('')
 
-        if operation in ['add', 'addition', 'plus', 'a']:
-            add_uncertainty()
+    # if operation in ['add', 'addition', 'plus', 'a']:
+    # add_uncertainty()
 
-        elif operation in ['subtract', 'minus', 'less', 's']:
-            subtract_uncertainty()
+    # elif operation in ['subtract', 'minus', 'less', 's']:
+    # subtract_uncertainty()
 
-        elif operation in ['multiply', 'mult', 'times', 'm']:
-            multiply_uncertainty()
+    # elif operation in ['multiply', 'mult', 'times', 'm']:
+    # multiply_uncertainty()
 
-        elif operation in ['divide', 'div', 'd']:
-            divide_uncertainty()
+    # elif operation in ['divide', 'div', 'd']:
+    # divide_uncertainty()
 
-        elif operation in ['square', 'exp', 'exponent']:
-            square_uncertainty()
+    # elif operation in ['square', 'exp', 'exponent']:
+    # square_uncertainty()
 
-        elif operation in ['no', 'cancel', 'n']:
-            print('Canceling')
-            quit()
+    # elif operation in ['no', 'cancel', 'n']:
+    # print('Canceling')
+    # quit()
 
-        else:
-            print('Invalid input')
+    # else:
+    # print('Invalid input')
 
-        flag = input('Continue? >>> ')
-        print('')
-        flag = flag.lower()
+    # flag = input('Continue? >>> ')
+    # print('')
+    # flag = flag.lower()
 
 
 if __name__ == '__main__':
